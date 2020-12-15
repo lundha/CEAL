8af4204f0f16a554b9d71153cde911412ab81abd
Martin Lund Haug <martlh@stud.ntnu.no>
2020-12-15 17:06:22 +0100

    updawe

--- CEAL.py
+++ CEAL.py
@@ -170,10 +170,10 @@ def test(model, device, criterion, test_loader, log_file, use_cifar):
                 loss = criterion(outputs, target.squeeze(1).long())
             test_loss += loss.item() * data.size(0)
             
-                if use_cifar == 1:
-                    acc, bacc, prec, recall, f1_score, rep = METRIX(target.long(), outputs)
-                else:
-                    acc, bacc, prec, recall, f1_score, rep = METRIX(target.squeeze(1).long(), outputs)
+            if use_cifar == 1:
+                acc, bacc, prec, recall, f1_score, rep = METRIX(target.long(), outputs)
+            else:
+                acc, bacc, prec, recall, f1_score, rep = METRIX(target.squeeze(1).long(), outputs)
             accuracy += acc
             balanced_accuracy += bacc
             precision += prec
